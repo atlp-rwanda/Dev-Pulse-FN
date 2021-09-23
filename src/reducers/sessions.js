54e8c87ca038a327419121c8e0d0c767780e24c9
Denis Niwemugisha <dniwemugisha@africa.cmu.edu>
2021-09-23 12:01:21 +0200

    Revert "Attendance toast messages"

--- src/reducers/sessions.js
+++ src/reducers/sessions.js
@@ -1,4 +1,4 @@
-import { FETCHING_SESSIONS_SUCCESS,CREATING_SESSIONS_SUCCESS,DELETING_SESSIONS_SUCCESS} from "../actions/actionType";
+import { FETCHING_SESSIONS_SUCCESS } from "../actions/actionType";
 
 
 const sessions = (state = { loading: true, sessions: [] }, { payload, type }) => {
@@ -10,21 +10,6 @@ const sessions = (state = { loading: true, sessions: [] }, { payload, type }) =>
         loading: false,
         sessions: payload
       }
-      case CREATING_SESSIONS_SUCCESS:
-        return{
-          ...state,
-          sessions:[...state.sessions,...payload]
-        }
-        case DELETING_SESSIONS_SUCCESS:
-          const currentSessions = state.sessions;
-          const toRemoveSessions = payload;
-          const remainingSessions = currentSessions.filter((currentSession) => toRemoveSessions.filter((toRem) => toRem === currentSession.id).length === 0);
-          console.log(remainingSessions,"ssssssssssssss")
-          
-          return {
-            ...state,
-            sessions: remainingSessions,
-          };
       default:
         return state
   }

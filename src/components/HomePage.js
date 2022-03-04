bcc14f3ac9efa58b3dbf9efaa52ef617b4acb7f1
cniyindagiriye <cniyindagiriye@gmail.com>
2022-03-04 13:59:28 +0200

    feat(ft-search) add search user \ - search user by name [done]

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,14 @@ import Table from './table';
 import ProgramDropDown from './shared/ProgramDropDown';
 import CohortDropDown from './shared/CohortDropDown';
 import BelowAverage from './shared/AverageDropDown';
+import SearchForm from './SearchForm';
 
 const HomePage = () => (
   <div className="container">
 <div className='tableHeading'>
  <div className='flex space-between items-center'>
  <div> My Developers Average Score</div>
+  <SearchForm placeholder={'Type name here'} />
   <Link className='btn mt-10' to='/ratings/rate/all'>Rate all</Link>
  </div>
 </div>
